1c4a57815530d94378dba3c311db97d422292cf5
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-03-06 22:02:17 +0200

    les 19. (Route, BrowserRouter, routing) & Adding new components

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Header from "./components/header/header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/dialogs/dialogs";
+import {Route, BrowserRouter} from 'react-router-dom'
+import News from "./components/news/news";
+import Music from "./components/music/music";
+import Settings from "./components/settings/settings";
+
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+                {/*<Profile/>*/}
             </div>
-            {/*<Profile/>*/}
-
-
-        </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,14 +3,13 @@ import React from 'react'
 import s from './navbar.module.css'
 
 const Navbar = () => {
-    return(
+    return (
         <nav className={s.nav}>
-            <div className={s.item}><a href='#'>Profile</a></div>
-            <div className='item'><a href='#'>Messages</a></div>
-            <div className='item'><a href='#'>News</a></div>
-            <div className='item'><a href='#'>Music</a></div>
-            <div className='item'><a href='#'>Settings</a></div>
-            <div className={s.item}><a>Huynya</a></div>
+            <div className={s.item}><a href='/profile'>Profile</a></div>
+            <div className='item'><a href='/dialogs'>Messages</a></div>
+            <div className='item'><a href='/news'>News</a></div>
+            <div className='item'><a href='/music'>Music</a></div>
+            <div className='item'><a href='/settings'>Settings</a></div>
         </nav>
     )
 }
