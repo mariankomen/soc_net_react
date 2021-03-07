7f0f5c34a4a9f1137197deaf950a0ef3c48a05a3
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-03-07 15:59:41 +0200

    les 26. UI BLL

--- src/components/dialogs/dialogs.js
+++ src/components/dialogs/dialogs.js
@@ -1,22 +1,9 @@
 import React from 'react'
 
+
+import DialogItem from './DialogItem/DialogItem'
+import Message from "./message/Message";
 import s from './dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-const Message = (props) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
 
 
 const Dialogs = () => {
