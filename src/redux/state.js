e1261288a7eb3d76d55ab0bfc0f2f1c2c01f68f0
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-03-09 22:27:09 +0200

    les 39. action creator, action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD_POST'
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT"
+
 let store = {
     _state: {
         profilePage: {
@@ -54,6 +57,14 @@ let store = {
         }
     }
 
+}
+
+export const addPostActionCreator = () => {
+    return {type: ADD_POST}
+}
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {type: UPDATE_NEW_POST_TEXT, newText: text}
 }
 
 export default store
