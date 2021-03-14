b7997097d3e5224d5208d8edcffe51659f244104
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-03-15 01:17:17 +0200

    les 54. componentDidMount

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -6,16 +6,13 @@ import userPhoto from '../../assets/images/user.png'
 
 export default class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => {
                 this.props.setUsers(res.data.items)
             })
     }
-
 
     render() {
         return (
